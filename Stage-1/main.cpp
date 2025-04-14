8d78e6d81f1e8e5ed5b8d4e3fbc1db2cd93aa891
Arun <arunofficial400@gmail.com>
2025-04-14 12:22:04 +0530

    added file-based-database

--- Stage-1/main.cpp
+++ Stage-1/main.cpp
@@ -9,7 +9,7 @@ class UrlShortener{
         int id = 0;
         string base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const string domain = "http://short.url/";
-
+        const string filePath = "data.txt";
         string encodeBase62(int num){
             string res;
             while(num>0){
@@ -21,7 +21,41 @@ class UrlShortener{
             }
             return res;
         }
+        void appendToFile(const string& shortKey,const string& longUrl){
+            ofstream outfile(filePath,ios::app);
+            outfile<<shortKey<<" "<<longUrl<<endl;
+        }
+        int decodeBase62(const string& str){
+            int num=0;
+            for(char c:str){
+                if('0'<=c && c<='9'){
+                    num = num*62 + (c-'0');
+                }else if('a'<=c && c<='z'){
+                    num = num*62 + (c-'a')+10;
+                }else if('A'<=c && c<='Z'){
+                    num = num*62 + (c-'A')+10+26;
+                }
+            }
+            return num;
+        }
+        void loadFromFile(){
+            ifstream infile(filePath);
+            string line;
+            while(getline(infile,line)){
+                istringstream iss(line);
+                string shortKey,longURL;
+                if(!(iss>>shortKey>>longURL))continue;
+                shortToLong[shortKey] = longURL;
+                longToShort[longURL] = shortKey;
+
+                int decodedId = decodeBase62(shortKey);
+                id = max(id,decodedId+1);
+            }
+        }
     public:
+        UrlShortener(){
+            loadFromFile();
+        }
         string shortenURL(const string& longUrl){
             if(longToShort.find(longUrl)!=longToShort.end()){
                 return domain+longToShort[longUrl];
@@ -29,6 +63,8 @@ class UrlShortener{
             string shortKey = encodeBase62(id++);
             longToShort[longUrl] = shortKey;
             shortToLong[shortKey] = longUrl;
+
+            appendToFile(shortKey,longUrl);
             return domain + shortKey;
         }
         string restoreURL(const string& shortUrl){
--- Stage-1/test.cpp
+++ Stage-1/test.cpp
@@ -3,8 +3,16 @@
     //methods --> shorten URL, restoreURL
 
 int main(){
-    UrlShortener test;
-    string s = "www.google.com";
-    cout<<test.shortenURL(s)<<endl;
-    cout<<test.shortenURL(s+"a")<<endl;
+    UrlShortener s;
+
+    string short1 = s.shortenURL("https://example.com/about-us");
+string short2 = s.shortenURL("https://openai.com/research");
+string short3 = s.shortenURL("https://github.com/arun-dev/URLShortener");
+
+// Retrieve original URLs
+cout << "Original 1: " << s.restoreURL(short1) << endl;
+cout << "Original 2: " << s.restoreURL(short2) << endl;
+cout << "Original 3: " << s.restoreURL(short3) << endl;
+
+return 0;
 }
